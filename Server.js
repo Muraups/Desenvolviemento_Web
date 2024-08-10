741589218c69cc43bbaba927d58c02f6e9fdc939
Murilo Augusto <140767549+Muraups@users.noreply.github.com>
2024-08-09 23:42:19 -0300

    Update Server.js
    Validação do Cep

--- Server.js
+++ Server.js
@@ -1,27 +1,36 @@
-const express =  require('express');
+const express = require('express');
 const axios = require('axios');
 
 const app = express();
 const port = 3000;
-
+ 
 app.get('/', (req, res) => {
-    res.send('Hello Word');
+    res.send('Hello world');
 });
 
+app.get('/route', (req, res) => {
+    res.send('Minha Primeira Rota');
+});
+
+ 
 app.get('/consulta-cep/:cep', async (req, res) => {
-    const cep = req.params.cep; //Obtendo o cep Url
-    console.log(cep);
-    
-    try {
-        //fazendo a requisição para a API do VIA CEP
-        const response = await  axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-        res.send(response.data)
-    } catch (erros){
-        console.error('Erro ao fazer requisição:', error);
-        res.status(500).send('Erro ao consultar CEP');
-     }
+    const cep = req.params.cep; // Obtendo o CEP da URL
+    const cepRegex = /^[0-9]{5}-?[0-9]{3}$/;
+    if (!cepRegex.test(cep)){
+        return res.status(400).send('CEP inválido. Formato: XXXXX-XXX');
+    }
+
+    try{
+        // Fazendo requisição para a API do ViaCEP
+        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+        res.json(response.data); // Retorna os dados da resposta
+    } catch (error) {
+        console.error('Erro ao fazer a requisição:', error);
+        res.status(500).send('Erro ao consultar o CEP');
+    }    
 });
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
+
 });
